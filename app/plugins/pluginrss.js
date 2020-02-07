1808f28d73e9816b48b49f434df941b432f0993f
o2urban@gmx.co.uk <o2urban@gmx.co.uk>
2020-02-07 15:25:24 +0000

    dev

--- app/plugins/pluginrss.js
+++ app/plugins/pluginrss.js
@@ -1,27 +1,38 @@
 var fs = require('fs-extra'),
   uuidv4 = require('uuid/v4'),
   comfunc = require(`${__base}app/comfunc.js`),
+  rssConverter = require('rss-converter'),
   _ = require('lodash'),
   moment = require('moment'),
   JsonDB = require('node-json-db'),
   dbrss = new JsonDB(`${__base}db/plugindb/cmsrss`, true, false);
 
-  try {
-    dbrss.reload();
-    var addata = dbrss.getData("/");
-    addata.forEach(function (value) {
-        if ( moment(value.LastRefresh).add(value.Hour,'h') < moment.utc())
-        {
-            // refresh time
+try {
+  dbrss.reload();
+  var addata = dbrss.getData("/");
+  addata.forEach(function (value) {
+    if (moment(value.LastRefresh).add(value.Hour, 'h') < moment.utc()) {
+      // refresh time
+      try {
+        // delete old post 
+        dbrsspost = new JsonDB(`${__base}db/plugindb/cmsrss_${value.Id}`, true, false);
+        var addata = dbrsspost.getData("/");
+        var filtered = addata.filter(function (value) { return moment(value.LastRefresh).add(value.DelPost, 'd') <= moment.utc(); });
+        dbrsspost.push("/", filtered);
 
-            // delete old post 
-            dbrsspost = new JsonDB(`${__base}db/plugindb/cmsrss_${value.Id}`, true, false);
-            var addata = dbrsspost.getData("/");
-            var filtered = addataAds.filter(function (value) { return moment(value.LastRefresh).add(value.DelPost,'d') <= moment.utc(); });
-            dbrsspost.push("/", filtered);
+        // load rssConverter
 
-        }
-        
-    });
-  }
-  catch (err) { console.log(err); }
+        (async () => {
+          let feed = await rssConverter.toJson(value.RSSurl);
+          console.log(feed);
+          var rssdata=[];
+          
+
+        })();
+      }
+      catch (err) { console.log(err); }
+    }
+
+  });
+}
+catch (err) { console.log(err); }
